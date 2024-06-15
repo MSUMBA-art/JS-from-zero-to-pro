a6e7157f8029cd1b78ac9227d3fd3b0460865111
ALEXANDER <msumba83@gmail.com>
2024-06-15 22:19:06 +0200

    function in a function

--- function.js
+++ function.js
@@ -1,5 +1,13 @@
-function function1() {
-    console.log('hello');
-    console.log(9 + 8);
+// function function1() {
+//     console.log('hello');
+//     console.log(9 + 8);
+// }
+// function1();
+
+// PARAMETERS 
+function calculateTax(cost, taxPercent = 0.1) {
+    console.log(cost * taxPercent)
 }
-function1();
+calculateTax(2000, 0.1)
+calculateTax(3000, 2)
+calculateTax(5000)
